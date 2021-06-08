b11acc22bc2964fcc66c71eabfa9b7f4d90d0f7d
scholtde <dewald@rc.technology>
2021-06-08 13:45:45 +0200

    Updated gst parameters

--- test.py
+++ test.py
@@ -60,9 +60,12 @@ capture_width = 1280
 capture_height = 720
 # Webcam
 
+# src = 'v4l2src device=/dev/video{} ! video/x-raw, width=(int){}, height=(int){} ! ' \
+#       'videoconvert !  video/x-raw, width=(int){}, height=(int){}, format=(string)BGR ! ' \
+#       'appsink'.format(0, capture_width, capture_height, width, height)
 src = 'v4l2src device=/dev/video{} ! video/x-raw, width=(int){}, height=(int){} ! ' \
       'videoconvert !  video/x-raw, width=(int){}, height=(int){}, format=(string)BGR ! ' \
-      'appsink'.format(0, capture_width, capture_height, width, height)
+      'appsink'.format(1, capture_width, capture_height, width, height)
 
 # CSI Camera
 # src = 'nvarguscamerasrc ! video/x-raw(memory:NVMM), width=%d, height=%d, ' \
